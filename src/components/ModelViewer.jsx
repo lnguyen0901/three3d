f122915bed6195cb4e3ac2c50d7d47fb49379fb2
LongNT <longnt@hotmail.com.vn>
2025-09-22 23:43:02 +0700

    update

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useGLTF, OrbitControls } from "@react-three/drei";
+import { useGLTF, OrbitControls, GizmoHelper, GizmoViewport } from "@react-three/drei";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleObjectSelection } from "../redux/store";
 import * as THREE from "three";
@@ -12,10 +12,12 @@ function Model({ modelPath }) {
   const { scene } = useGLTF(modelPath);
   const selectedObjects = useSelector((state) => state.selection.selectedObjects);
   const dispatch = useDispatch();
+
   const handleMeshClick = (event) => {
     event.stopPropagation();
-    const objectName = event.object.name;
-    dispatch(toggleObjectSelection({ objectName }));
+    const objectName = event.object?.name;
+    console.log("clicked:", objectName);
+    if (objectName) dispatch(toggleObjectSelection({ objectName }));
   };
 
   // Clone the scene and update materials on the clone so we don't mutate the original GLTF
@@ -56,12 +58,61 @@ function Model({ modelPath }) {
 export default function ModelViewer({ modelPath }) {
   return (
     <div style={{ height: "500px", width: "100%" }}>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
+      <Canvas gl={{ alpha: false }} camera={{ position: [-45, 2.5, -5], fov: 50 }}>
+        <ambientLight intensity={1} />
+        <directionalLight color={0xaaaaaa} position={[5, 10, 5]} intensity={2} />
+        <directionalLight color={0xaaaaaa} position={[30, 45, 30]} intensity={10} />
+        <directionalLight color={0xaaaaaa} position={[30, 45, 30]} intensity={2} />
+        {/* Set three.js scene background to black */}
+        <color attach="background" args={[0, 0, 0]} />
+        {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} /> */}
         <Model modelPath={modelPath} />
         <OrbitControls />
+
+        {/* Viewport gizmo: shows orientation in the corner and allows quick reorientation */}
+        <GizmoHelper alignment="top-right" margin={[80, 80]}>
+          <GizmoViewport labelColor="black">
+            {/* Custom axis arrows: X (red), Y (green), Z (blue) */}
+            <group position={[0, 0, 0]} scale={0.6}>
+              {/* X axis arrow */}
+              <group position={[0.6, 0, 0]} rotation={[0, 0, -Math.PI / 2]}>
+                <mesh>
+                  <cylinderBufferGeometry args={[0.02, 0.02, 0.5, 8]} />
+                  <meshBasicMaterial color="red" />
+                </mesh>
+                <mesh position={[0.3, 0, 0]}>
+                  <coneBufferGeometry args={[0.06, 0.12, 8]} />
+                  <meshBasicMaterial color="red" />
+                </mesh>
+              </group>
+
+              {/* Y axis arrow */}
+              <group position={[0, 0.6, 0]} rotation={[0, 0, 0]}>
+                <mesh>
+                  <cylinderBufferGeometry args={[0.02, 0.02, 0.5, 8]} />
+                  <meshBasicMaterial color="green" />
+                </mesh>
+                <mesh position={[0, 0.3, 0]}>
+                  <coneBufferGeometry args={[0.06, 0.12, 8]} />
+                  <meshBasicMaterial color="green" />
+                </mesh>
+              </group>
+
+              {/* Z axis arrow */}
+              <group position={[0, 0, 0.6]} rotation={[Math.PI / 2, 0, 0]}>
+                <mesh>
+                  <cylinderBufferGeometry args={[0.02, 0.02, 0.5, 8]} />
+                  <meshBasicMaterial color="blue" />
+                </mesh>
+                <mesh position={[0, 0, 0.3]}>
+                  <coneBufferGeometry args={[0.06, 0.12, 8]} />
+                  <meshBasicMaterial color="blue" />
+                </mesh>
+              </group>
+            </group>
+          </GizmoViewport>
+        </GizmoHelper>
       </Canvas>
     </div>
   );
